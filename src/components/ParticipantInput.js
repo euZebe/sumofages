15f962cec13ece6420053807f2d1cedca07a87b8
euZebe <euzebe@axiome.me>
2017-11-10 07:23:27 +0100

    handle day and month on 1 digit + change date_format when language changes

--- src/components/ParticipantInput.js
+++ src/components/ParticipantInput.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { translate } from 'react-i18next'
+import i18next from 'i18next'
 import { Input } from 'reactstrap'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css';
-
-const DATE_FORMAT = 'DD/MM/YYYY';
 
 class ParticipantInput extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = { name: '', dateOfBirth: null };
+        this.DATE_FORMAT = props.t('date_format_accepted');
+
+        i18next.on('languageChanged', (lng) => this.DATE_FORMAT = props.t('date_format_accepted'));
     }
 
     handleValueChanged = (field) => {
@@ -44,7 +46,7 @@ class ParticipantInput extends React.Component {
                     selected={dateOfBirth}
                     onChange={this.handleDateOfBirthChanged}
                     className='form-control'
-                    dateFormat={DATE_FORMAT}
+                    dateFormat={this.DATE_FORMAT}
                     placeholderText={t('date_of_birth_label')}
                 />
             </div>
